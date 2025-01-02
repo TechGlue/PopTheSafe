d2c03c1c600a887778ca4ef406de6e6e1efe541b
TechGlue <garcialuis42@outlook.com>
2025-01-01 23:02:20 -0800

    adming code generation logic + tests

--- Safe/Safe/Models/MySafe.cs
+++ Safe/Safe/Models/MySafe.cs
@@ -15,93 +15,105 @@ public class MySafe
 
     // note the password can only be 4 digits don't want that being too long
     // don't need it in the constructor
-    public string Password { get; set; } = "0000";
+    public string Password { get; private set; } = "0000";
 
     public string AdminPassword { get; private set; } = "0000";
 
     public DateTime CreationTime { get; private set; } = DateTime.Now;
 
     public DateTime LastPasswordUpdateTime { get; private set; } = DateTime.Now;
-    
+
     public DateTime LastAdminPasswordUpdateTime { get; private set; } = DateTime.Now;
 
     // open or closed states - the trigger is a char
     public StateMachine<string, char> SafeState = new StateMachine<string, char>("open");
 
-    
     public void ChangeSafePassword(string password)
     {
-        // do work to ensure the password is of 4 digits. throw exceptions
+        // Ensure the password is of 4 digits. throw exceptions
         if (VerifyFourDigitCode(password) is not true)
         {
             throw new ArgumentException("password is not valid ");
         }
-        
-        // update the password 
+
+        // update the admin password for the safe
         AdminPassword = CalculateAdminCode(password);
-        
-        // update pass word. 
+
+        // update safe pass word with the user password 
         Password = password;
-        
-        // finally update the times 
+
+        // update the times for logging purposes
         LastPasswordUpdateTime = DateTime.Now;
         LastAdminPasswordUpdateTime = DateTime.Now;
     }
 
+    // Think about: Making the limits flexible to allow better testing. For a Proof of concept this is fine.   
     public string CalculateAdminCode(string password)
     {
-        int userPassword = -1; 
-        
+        int userPassword = -1;
+
         if (Int32.TryParse(password, out userPassword) is false)
         {
             throw new ArgumentException("Invalid password. Not able to generate admin code");
         }
-        
-        Func<int, (int lowerLimit, int upperLimits)> calclimits = ((x) =>
-        {
-            int lowerLimit = x - 750;
-            if (lowerLimit < 0)
-            {
-                lowerLimit = 0;
-            }
 
-            int upperLimit = x + 750;
-
-            if (upperLimit > 9999)
-            {
-                upperLimit = 9999;
-            }
-
-            return (lowerLimit,upperLimit);
-        });
-
-        var adminPassLimits = calclimits(userPassword);
+        var adminPassLimits = CalcLimits(userPassword);
 
         Random rnd = new Random();
 
         // Probably can optimize this block
         int adminPassGenerator = rnd.Next(0, 9999);
 
-        while (adminPassGenerator < adminPassLimits.lowerLimit && adminPassGenerator > adminPassLimits.upperLimits)
+        while ((Enumerable.Range(adminPassLimits.lowerLimit, userPassword).Contains(adminPassGenerator) &&
+                (Enumerable.Range(userPassword, adminPassLimits.upperLimits).Contains(adminPassGenerator))))
         {
             adminPassGenerator = rnd.Next(0, 9999);
         }
-        
-        // fill in leading 0's 
-        int curPasswordSize = adminPassGenerator.ToString().Length; 
-        
+
+        // Fill in leading 0's 
+        int curPasswordSize = adminPassGenerator.ToString().Length;
+
         StringBuilder adminPasswordBuilder = new StringBuilder();
-        
+
         while (curPasswordSize < 4)
         {
             adminPasswordBuilder.Append(0);
             curPasswordSize++;
         }
-        
-        // append the password or the final portion;
+
+        // Append the password or the final portion;
         adminPasswordBuilder.Append(adminPassGenerator);
 
         return adminPasswordBuilder.ToString();
+    }
+
+
+    /* Criteria 
+     * The admin code doesn't appear to be closer than 750 to the chosen code.  So for example if I pick 1000 as the code, the admin code doesn't appear to be anywhere in the range 250-1000 or 1000-1750.  It wraps around so if I choose 0250 as the code,
+     *
+     * The case below is not correct it'd be 9499 - 0250
+     * it wouldn't pick 9500-0250 or 250-1000.
+     *
+     */
+    public (int lowerLimit, int upperLimits) CalcLimits(int x)
+    {
+        // In a perfect world the lower and upper limit will be described. Keep in mind sometimes it will be greater. 
+        int lowerLimit = x - 750;
+        if (lowerLimit < 0)
+        {
+            // wrap around the difference
+            lowerLimit += 9999;
+        }
+
+        int upperLimit = x + 750;
+
+        if (upperLimit > 9999)
+        {
+            // wrap around the difference
+            upperLimit -= 9999;
+        }
+
+        return (lowerLimit, upperLimit);
     }
 
     public bool VerifyFourDigitCode(string password)
--- Safe/SafeTesting/SafeTests.cs
+++ Safe/SafeTesting/SafeTests.cs
@@ -1,12 +1,21 @@
 using Safe.Models;
+using Xunit.Abstractions;
 
 namespace SafeTesting;
 
 public class SafeTests
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public SafeTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Theory]
-    [InlineData("1023")]
-    [InlineData("0000")]
+    [InlineData("1")]
+    [InlineData("01")]
+    [InlineData("123")]
     [InlineData("9999")]
     public void CalculateAdminCode_FourDigitPassword_ReturnsFourDigitPassword(string password)
     {
@@ -19,7 +28,41 @@ public class SafeTests
         // Assert
         Assert.Equal(4, adminCode.Length);
     }
-    
+
+    [Fact]
+    public void CalculateAdminCode_FourDigitPassword_ThrowsArgumentException()
+    {
+        // Arrange 
+        MySafe hotelSafe = new MySafe("MySafe");
+        string password = "NotAPassword";
+
+        // Assert
+        Assert.Throws<ArgumentException>(() => hotelSafe.CalculateAdminCode(password));
+    }
+
+
+    [InlineData("01")]
+    [InlineData("9999")]
+    [InlineData("0250")]
+    [InlineData("123")]
+    [Theory]
+    public void CalculateAdminCode_FourDigitPassword_ReturnsFourDigitPasswordInRange(string safePassword)
+    {
+        // Arrange 
+        MySafe hotelSafe = new MySafe("MySafe");
+
+        int passcode = Int32.Parse(safePassword);
+        var limits = hotelSafe.CalcLimits(passcode);
+
+        // Act 
+        string adminCode = hotelSafe.CalculateAdminCode(safePassword);
+
+        // Assert
+        Assert.True(!(Enumerable.Range(limits.lowerLimit, passcode).Contains(Int32.Parse(adminCode)) &&
+                      !Enumerable.Range(passcode, limits.upperLimits).Contains(Int32.Parse(adminCode))));
+        Assert.Equal(4, adminCode.Length);
+    }
+
     [Theory]
     [InlineData("1023")]
     [InlineData("0000")]
@@ -35,7 +78,7 @@ public class SafeTests
         // Assert
         Assert.True(output);
     }
-    
+
     [Theory]
     [InlineData("1")]
     [InlineData("03")]
