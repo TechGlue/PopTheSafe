a3abc643a347de4f58f3996376ec6ee2d6da68fd
TechGlue <garcialuis42@outlook.com>
2025-01-21 00:50:57 -0800

    added refactors. moved menu out of the service.

--- Safe/MySafe/Safe/MySafeConsole.cs
+++ Safe/MySafe/Safe/MySafeConsole.cs
@@ -2,27 +2,64 @@ using Spectre.Console;
 
 namespace Safe;
 
-public class MySafeConsole:IMySafeConsole
+public class MySafeConsole : IMySafeConsole
 {
     private readonly IAnsiConsole _console = AnsiConsole.Console;
 
-    public void Write(string text)
+    public bool SafeMenu(ISafe safe)
     {
-        _console.Write(text);
-    }
+        var actions = new Dictionary<int, Func<ISafe, SafeResponse>>()
+        {
+            { 1, safe => safe.Open() },
+            { 2, safe => safe.Close() },
+            {
+                3, safe =>
+                {
+                    Console.Write("\nEnter Safe PIN: ");
+                    var pin = Console.ReadLine() ?? "";
+                    SafeResponse safeResponse = new SafeResponse();
 
-    public void Markup(string markup)
-    {
-        _console.Markup(markup);
-    }
+                    safe.SetCode(pin, result => safeResponse = result);
+                    return safeResponse;
+                }
+            },
+            { 4, safe => safe.PressReset() },
+            { 5, safe => safe.PressLock() },
+        };
 
-    public string Prompt(TextPrompt<string> prompt)
-    {
-        return _console.Prompt(prompt);
-    }
+        try
+        {
+            _console.WriteLine("\n" + safe.Describe());
+            _console.WriteLine("What will you do?");
+            _console.WriteLine("1) Open the door");
+            _console.WriteLine("2) Close the door");
+            _console.WriteLine("3) Enter a pin");
+            _console.WriteLine("4) Press reset");
+            _console.WriteLine("5) Press lock");
 
-    public void WriteTable(Table table)
-    {
-        _console.Write(table);
+
+            var action = int.TryParse(Console.ReadKey().KeyChar.ToString(), out int choice) switch
+            {
+                true => actions.ContainsKey(choice) switch
+                {
+                    true => actions[choice],
+                    false => actions[-1],
+                },
+                false => actions[-1],
+            };
+
+            var result = action(safe);
+
+            if (!result.isSuccessful)
+            {
+                Console.WriteLine($"Failed: {result.isDetail}");
+            }
+        }
+        catch (KeyNotFoundException)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
--- Safe/MySafe/SafeHostedService.cs
+++ Safe/MySafe/SafeHostedService.cs
@@ -3,8 +3,6 @@ using Microsoft.Extensions.Logging;
 
 namespace Safe;
 
-// IHosted service is not good at having long-running tasks in the StartAsync because the application will not fully startup
-// Avoid long-running tasks in Start Async 
 public class SafeHostedService : BackgroundService
 {
     private readonly ISafe _safe;
@@ -21,77 +19,13 @@ public class SafeHostedService : BackgroundService
         _logger = logger;
     }
 
-    private Task<bool> SafeMenu()
-    {
-        var actions = new Dictionary<int, Func<ISafe, SafeResponse>>()
-        {
-            { 1, safe => safe.Open() },
-            { 2, safe => safe.Close() },
-            {
-                3, safe =>
-                {
-                    Console.Write("\nEnter Safe PIN: ");
-                    var pin = Console.ReadLine() ?? "";
-                    SafeResponse safeResponse = new SafeResponse();
-
-                    safe.SetCode(pin, result => safeResponse = result);
-                    return safeResponse;
-                }
-            },
-            { 4, safe => safe.PressReset() },
-            { 5, safe => safe.PressLock() },
-        };
-
-        try
-        {
-            Console.WriteLine("\n" + _safe.Describe());
-            Console.WriteLine("What will you do?");
-            Console.WriteLine("1) Open the door");
-            Console.WriteLine("2) Close the door");
-            Console.WriteLine("3) Enter a pin");
-            Console.WriteLine("4) Press reset");
-            Console.WriteLine("5) Press lock");
-
-
-            var action = int.TryParse(Console.ReadKey().KeyChar.ToString(), out int choice) switch
-            {
-                true => actions.ContainsKey(choice) switch
-                {
-                    true => actions[choice],
-                    false => actions[-1],
-                },
-                false => actions[-1],
-            };
-
-            var result = action(_safe);
-
-            if (!result.isSuccessful)
-            {
-                Console.WriteLine($"Failed: {result.isDetail}");
-            }
-        }
-        catch (KeyNotFoundException)
-        {
-            return Task.FromResult(false);
-        }
-
-        return Task.FromResult(true);
-    }
-
-
-    // This is where long-running loads should be stored
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        // Await for 200ms to ensure startup
+        // 200ms delay to ensure application start up logs
         await Task.Delay(200, stoppingToken);
 
-        // Loop menu
-        bool activeMenu = true;
-
-        while (!stoppingToken.IsCancellationRequested && activeMenu)
-        {
-            activeMenu = await SafeMenu();
-        }
+        await Task.Yield();
+        while (!stoppingToken.IsCancellationRequested && _console.SafeMenu(_safe)) ;
 
         _lifetime.StopApplication();
     }
